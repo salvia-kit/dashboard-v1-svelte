8e409166fffb69ce2e68e36d9f374196b4933d0f
enochndika <enochbeloved@gmail.com>
2021-12-22 04:32:55 +0100

    first major version

---
+++ src/dashboard/Layout.svelte
@@ -0,0 +1,63 @@
+<script>
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import { browser } from '$app/env';
+	import Overlay from './provider/Overlay.svelte';
+	import { closeSidenav } from './provider/store';
+	import TopNavigation from './topnavigation/Index.svelte';
+	import SideNavigation from './sidenavigation/Index.svelte';
+
+	if (browser) {
+		page.subscribe(() => {
+			// close side navigation when route changes. it's triggered when viewport is less than 1024px
+			if (window.innerWidth < 1024) {
+				closeSidenav();
+			}
+		});
+	}
+
+	onMount(() => {
+		document.getElementsByTagName('body').item(0).removeAttribute('tabindex');
+	});
+</script>
+
+<!-- w-[calc(100%-16rem)] class get the remain width of the main tag from lg:viewport by dividing
+(the total width by the width of the side navigation component which is w-64 = 16rem)-->
+
+<div class="bg-mac bg-center bg-cover h-screen overflow-hidden w-full lg:p-4">
+	<div class="content h-screen overflow-hidden relative lg:rounded-2xl">
+		<div class="flex items-start">
+			<Overlay />
+			<SideNavigation mobilePosition="right" />
+			<div class="flex flex-col h-screen pl-0 w-full lg:space-y-4 lg:w-[calc(100%-16rem)]">
+				<TopNavigation />
+				<main class="main h-screen pb-36 pt-4 px-2 md:pb-8 md:px-4 lg:px-6">
+					<slot />
+				</main>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.content {
+		background-color: rgba(16 18 27 / 40%);
+		backdrop-filter: blur(24px);
+	}
+
+	.main {
+		color: #f9fafb;
+		background-color: rgba(16 18 27 / 40%);
+		overflow: auto;
+	}
+
+	.main::-webkit-scrollbar {
+		width: 6px;
+		border-radius: 10px;
+	}
+
+	.main::-webkit-scrollbar-thumb {
+		background: rgb(1 2 3 / 40%);
+		border-radius: 10px;
+	}
+</style>
---
+++ src/dashboard/sidenavigation/Items.svelte
@@ -0,0 +1,27 @@
+<script>
+	import data from './data';
+	import { page } from '$app/stores';
+
+	const style = {
+		title: `mx-4 text-sm`,
+		section: `font-medium mb-4 pl-5 text-gray-500 text-lg lg:pl-6`,
+		link: `flex items-center justify-start my-1 p-3 text-white w-full`,
+		active: `border-white lg:border-red-300 border-l-4 lg:border-l-0 lg:border-r-4`
+	};
+</script>
+
+<ul class="mt-6 md:pl-6">
+	<li>
+		{#each data as section, i (section.section)}
+			<div class="mb-10">
+				<div class={style.section}>{section.section}</div>
+				{#each section.content as item, i (item.title)}
+					<a href={item.link} class={`${style.link} ${item.link === $page.path && style.active}`}>
+						<span><svelte:component this={item.icon} /></span>
+						<span class={style.title}>{item.title}</span>
+					</a>
+				{/each}
+			</div>
+		{/each}
+	</li>
+</ul>
